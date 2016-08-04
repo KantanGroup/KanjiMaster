6c5a79e31da5fcc320fcf3cbffb81af927824391
Tran Anh Tuan <tk1cntt@gmail.com>
2016-08-04 10:38:19 +0900

    Add animatable to FlashCard

--- App/Components/FlashCard.js
+++ App/Components/FlashCard.js
@@ -1,58 +1,86 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { TouchableOpacity, Text } from 'react-native'
 import styles from './Styles/FlashCardStyle'
 
-var cardDefault = {}
-cardDefault.definition = 'Kanji';
-cardDefault.kanji = '漢字';
-cardDefault.hiragana = 'かんじ';
+import * as Animatable from 'react-native-animatable'
 
-var cardDefaultSetting = {}
-cardDefaultSetting.definition = true;
-cardDefaultSetting.hiragana = true;
+var cardDefault = {
+  definition: 'Kanji',
+  kanji: '漢字',
+  hiragana: 'かんじ'
+}
+
+var cardSetting = {
+  definition: true,
+  hiragana: true
+}
+
+var delay = 500;
+var animation = 'bounceIn';
 
 export default class FlashCard extends React.Component {
 
   // Prop type warnings
   static propTypes = {
     card: React.PropTypes.object,
+    onPress: React.PropTypes.func,
     setting: React.PropTypes.object
   }
 
   // Defaults for props
   static defaultProps = {
     card: cardDefault,
-    setting: cardDefaultSetting
+    setting: cardSetting
   }
 
   render () {
     if (this.props.setting.definition && this.props.setting.hiragana) {
       return (
-        <View style={styles.card}>
-          <Text numberOfLines={2} ref="definition" style={styles.definition}>{this.props.card.definition}</Text>
-          <Text numberOfLines={1} style={styles.kanji}>{this.props.card.kanji}</Text>
-          <Text ref="pinyin" numberOfLines={1} style={styles.hiragana}>{this.props.card.hiragana}</Text>
-        </View>
+        <Animatable.View
+          delay={delay}
+          animation={animation}
+        >
+          <TouchableOpacity style={styles.card} onPress={this.props.onPress}>
+            <Text numberOfLines={2} ref="definition" style={styles.definition}>{this.props.card.definition}</Text>
+            <Text numberOfLines={1} style={styles.kanji}>{this.props.card.kanji}</Text>
+            <Text ref="pinyin" numberOfLines={1} style={styles.hiragana}>{this.props.card.hiragana}</Text>
+          </TouchableOpacity>
+        </Animatable.View>
       )
     } else if (this.props.setting.definition && !this.props.setting.hiragana) {
       return (
-        <View style={styles.card}>
-          <Text numberOfLines={2} ref="definition" style={styles.definition}>{this.props.card.definition}</Text>
-          <Text numberOfLines={1} style={styles.kanji}>{this.props.card.kanji}</Text>
-        </View>
+        <Animatable.View
+          delay={delay}
+          animation={animation}
+        >
+          <TouchableOpacity style={styles.card} onPress={this.props.onPress}>
+            <Text numberOfLines={2} ref="definition" style={styles.definition}>{this.props.card.definition}</Text>
+            <Text numberOfLines={1} style={styles.kanji}>{this.props.card.kanji}</Text>
+          </TouchableOpacity>
+        </Animatable.View>
       )
     } else if (!this.props.setting.definition && this.props.setting.hiragana) {
       return (
-        <View style={styles.card}>
-          <Text numberOfLines={1} style={styles.kanji}>{this.props.card.kanji}</Text>
-          <Text ref="pinyin" numberOfLines={1} style={styles.hiragana}>{this.props.card.hiragana}</Text>
-        </View>
+        <Animatable.View
+          delay={delay}
+          animation={animation}
+        >
+          <TouchableOpacity style={styles.card} onPress={this.props.onPress}>
+            <Text numberOfLines={1} style={styles.kanji}>{this.props.card.kanji}</Text>
+            <Text ref="pinyin" numberOfLines={1} style={styles.hiragana}>{this.props.card.hiragana}</Text>
+          </TouchableOpacity>
+        </Animatable.View>
       )
     } else {
       return (
-        <View style={styles.card}>
-          <Text numberOfLines={1} style={styles.kanji}>{this.props.card.kanji}</Text>
-        </View>
+        <Animatable.View
+          delay={delay}
+          animation={animation}
+        >
+          <TouchableOpacity style={styles.card} onPress={this.props.onPress}>
+            <Text numberOfLines={1} style={styles.kanji}>{this.props.card.kanji}</Text>
+          </TouchableOpacity>
+        </Animatable.View>
       )
     }
   }
