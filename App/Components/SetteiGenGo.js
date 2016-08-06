26b936579967ee1a63254fdc0b01a34b05e8be3b
Tran Anh Tuan <tk1cntt@gmail.com>
2016-08-06 12:38:08 +0900

    Add setting language

---
+++ App/Components/SetteiGenGo.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+//import styles from './Styles/SetteiGenGoStyle'
+
+import { RadioButtons, SegmentedControls } from 'react-native-radio-buttons';
+
+export default class SetteiGenGo extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  renderChoiseLanguage(){
+    const options = [
+      {
+        language: 'English',
+        value: 'en'
+      },
+      {
+        language: '日本語',
+        value: 'ja'
+      },
+      {
+        language: 'Tiếng Việt',
+        value: 'vi'
+      }
+    ];
+
+    function setSelectedOption(option){
+      this.setState({
+        selectedLanguage: option,
+      });
+    }
+
+    return (
+      <View>
+        <SegmentedControls
+          direction={'column'}
+          tint= {'#007AFF'}
+          selectedTint= {'white'}
+          options={ options }
+          onSelection={ setSelectedOption.bind(this) }
+          selectedOption={ this.state.selectedLanguage }
+          extractText={ (option) => option.language }
+          testOptionEqual={ (a, b) => {
+            if (!a || !b) {
+              return false;
+            }
+            return a.language === b.language
+          }}
+        />
+        <Text style={{marginTop: 10}}>
+          Selected option:
+          {
+            this.state.selectedLanguage && this.state.selectedLanguage.value || 'none'
+          }
+        </Text>
+      </View>);
+  }
+
+  render () {
+    return (
+      <View>
+        {this.renderChoiseLanguage()}
+      </View>
+    )
+  }
+}
