7cec6fb286567bbbc9c707afd38d07c6339d304b
Tran Anh Tuan <tk1cntt@gmail.com>
2016-08-11 08:28:18 +0900

    Remove react-native-svg

--- android/app/src/main/java/com/kanjimaster/MainApplication.java
+++ android/app/src/main/java/com/kanjimaster/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.RNSvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -28,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNSvgPackage(),
             new VectorIconsPackage(),
             new ReactNativeI18n(),
             new RNDeviceInfo()
