81413b6933f4f626ce2bb9bb4b2623302a8a1006
Tran Anh Tuan <tk1cntt@gmail.com>
2016-08-14 21:08:58 +0900

    Add star assets

--- App/Themes/Images.js
+++ App/Themes/Images.js
@@ -1,5 +1,12 @@
 // leave off @2x/@3x
 const images = {
+  bg: require('../Images/BG.png'),
+  emptyStar: require('../Images/empty-star.png'),
+  fullStar: require('../Images/full-star.png'),
+  igniteLogo: require('../Images/ignite_logo.png'),
+  ir: require('../Images/ir.png'),
+  tileBg: require('../Images/tile_bg.png'),
+  topLogo: require('../Images/top_logo.png'),
   logo: require('../Images/ir.png'),
   clearLogo: require('../Images/top_logo.png'),
   ignite: require('../Images/ignite_logo.png'),
