7b230d158e775c9ec402009e59efc007a3ccfa87
Tran Anh Tuan <tk1cntt@gmail.com>
2016-08-14 20:16:42 +0900

    Add Star component

---
+++ App/Components/Styles/StarStyle.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+import { Colors, Metrics, ApplicationStyles } from '../../Themes/'
+
+export default StyleSheet.create({
+  star: {
+    flex: 1,
+    width: 15,
+    height: 15,
+  },
+  stars: {
+    marginHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+})
