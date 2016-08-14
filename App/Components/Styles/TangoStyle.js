f2c61d29c6b7a4a085035e5ba4f405759bcd877b
Tran Anh Tuan <tk1cntt@gmail.com>
2016-08-14 18:00:53 +0900

    Update Tango

--- App/Components/Styles/TangoStyle.js
+++ App/Components/Styles/TangoStyle.js
@@ -15,8 +15,69 @@ export default StyleSheet.create({
   },
   row: {
     flex: 1,
-    backgroundColor: Colors.fire,
+    //backgroundColor: Colors.fire,
     marginVertical: Metrics.smallMargin,
-    justifyContent: 'center'
+    //justifyContent: 'center',
+    paddingVertical: 0,
+    paddingHorizontal: 0,
+  },
+  separator: {
+    marginHorizontal: 20,
+  },
+  title: {
+    //paddingVertical: 5,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+  },
+  symbol: {
+    fontSize: 15,
+    lineHeight: 21,
+    width: 22,
+    color: '#99A7B9',
+  },
+  description: {
+    //padding: 14,
+    //paddingLeft: 20 + 22,
+    flex: 1,
+    flexDirection: 'row',
+  },
+  text: {
+    fontSize: 15,
+    lineHeight: 21,
+    color: '#002350',
+    //flex: 1,
+  },
+  leftItem: {
+    //flex: 1,
+    //alignItems: 'flex-start',
+  },
+  centerItem: {
+    flex: 2,
+    alignItems: 'center',
+  },
+  rightItem: {
+    flex: 1,
+    //alignItems: 'flex-end',
+  },
+  language: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    width: 40,
+  },
+  languageItem: {
+    textAlign: 'center',
+    padding: 5,
+  },
+  meaning: {
+    paddingLeft: 10,
+    lineHeight: 21,
+    color: '#002350',
+  },
+  icon: {
+    width: 300,
+    height: 300,
+    alignSelf: 'center',
   },
 })
--- App/Components/Tango.js
+++ App/Components/Tango.js
@@ -1,6 +1,12 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { TouchableWithoutFeedback, TouchableOpacity, ScrollView, View, Text } from 'react-native'
 import styles from './Styles/TangoStyle'
+
+import { RadioButtons } from 'react-native-radio-buttons';
+
+import F8Touchable from './f8/F8Touchable'
+
+var LayoutAnimation = require('LayoutAnimation');
 
 export default class Tango extends React.Component {
 
@@ -9,11 +15,138 @@ export default class Tango extends React.Component {
   }
 
   render () {
+    let languages = [];
+    let meanings =  [];
+
+    this.props.tango.meanings.map((meaning) => {
+      languages.push(meaning.language);
+      meanings[meaning.language] = meaning[meaning.language];
+    })
+
     return (
-      <View style={styles.row}>
-        <Text style={styles.boldLabel}>{this.props.tango.tango}</Text>
-        <Text style={styles.label}>{this.props.tango.hiragana}</Text>
+      <View>
+        <TangoComponent
+          languages={languages}
+          tango={this.props.tango.tango}
+          hiragana={this.props.tango.hiragana}
+          hanViet={this.props.tango.hanViet}
+          meanings={meanings}/>
       </View>
     )
+  }
+}
+
+class TangoComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+    };
+  }
+
+  componentWillMount(){
+    this.setState({
+      selectedOption: this.props.languages[1] || 'ja',
+      selectedMeaning: this.props.meanings[this.props.languages[1] || 'ja']
+    });
+  }
+
+  renderChoiseLanguage() {
+    function setSelectedOption(selectedOption){
+      this.setState({
+        selectedOption,
+        selectedMeaning: this.props.meanings[selectedOption]
+      });
+    }
+
+    function renderOption(option, selected, onSelect, index){
+      const style = selected ? { fontWeight: 'bold', color: 'blue'} : {};
+
+      return (
+        <TouchableOpacity onPress={onSelect} key={index}>
+          <Text style={[styles.languageItem, style]}>{option}</Text>
+        </TouchableOpacity>
+      );
+    }
+
+    function renderContainer(optionNodes){
+      return <View style={[styles.language, { flexDirection: 'column', justifyContent: 'space-between'}]}>{optionNodes}</View>;
+    }
+
+    return (
+      <View>
+        <RadioButtons
+          options={ this.props.languages }
+          onSelection={ setSelectedOption.bind(this) }
+          selectedOption={this.state.selectedOption }
+          renderOption={ renderOption }
+          renderContainer={ renderContainer }
+          onSelect={ this.switchLanguage(this.state.selectedOption)}
+        />
+      </View>);
+  }
+
+  switchLanguage(selectedlanguage) {
+    function setSelectedOption(selectedlanguage){
+      this.setState({
+        selectedMeaning: this.props.meanings[selectedlanguage]
+      });
+    }
+  }
+
+  render() {
+    var description;
+    if (this.state.expanded) {
+      description = (
+        <View style={styles.description}>
+          {this.renderChoiseLanguage()}
+          <View style={styles.rightItem}>
+            <Text style={styles.meaning}>
+              {
+                this.state.selectedMeaning[0].meaning
+              }
+            </Text>
+          </View>
+        </View>
+      );
+    }
+    var hiragana;
+    if (this.props.tango !== this.props.hiragana) {
+      hiragana = (
+        <Text style={styles.text}>
+          [{this.props.hiragana}]
+        </Text>
+      )
+    }
+    var hanViet;
+    if (this.props.hanViet) {
+      hanViet = (
+        <Text style={styles.text}>
+          [{this.props.hanViet}]
+        </Text>
+      )
+    }
+    return (
+      <View style={styles.row}>
+        <F8Touchable onPress={() => this.toggle()}>
+          <View style={styles.title} >
+            <Text style={styles.symbol}>
+              {this.state.expanded ? '\u2212' : '+'}
+            </Text>
+            <Text style={styles.text}>
+              {this.props.tango}
+            </Text>
+            {hiragana}
+            {hanViet}
+          </View>
+        </F8Touchable>
+        {description}
+      </View>
+    );
+  }
+
+  toggle() {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    this.setState({expanded: !this.state.expanded});
   }
 }
