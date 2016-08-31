f128c1367fce4df39345d155d27b04d52f7cc947
Tran Anh Tuan <tk1cntt@gmail.com>
2016-08-31 21:16:37 +0900

    Update kanji db

--- App/Store/RealmDatabase.js
+++ App/Store/RealmDatabase.js
@@ -5,16 +5,16 @@ const KanjiMatomeSchema = {
   primaryKey: 'keyword',
   properties: {
     keyword:      {type: 'string'},
-    hanViet:      {type: 'string', optional: true},
-    onyomi:       {type: 'string', optional: true},
-    kunyomi:      {type: 'string', optional: true},
-    radical:      {type: 'string', optional: true},
-    strokes:      {type: 'int', optional: true},
-    grade:        {type: 'int', optional: true},
-    jlpt:         {type: 'int', optional: true},
-    jouyou:       {type: 'int', optional: true},
-    rtk6th:       {type: 'int', optional: true},
-    rank:         {type: 'int', optional: true}
+    hanViet:      {type: 'string', optional: true, indexed: true},
+    onyomi:       {type: 'string', optional: true, indexed: true},
+    kunyomi:      {type: 'string', optional: true, indexed: true},
+    radical:      {type: 'string', optional: true, indexed: true},
+    strokes:      {type: 'int', optional: true, indexed: true},
+    grade:        {type: 'int', optional: true, indexed: true},
+    jlpt:         {type: 'int', optional: true, indexed: true},
+    jouyou:       {type: 'int', optional: true, indexed: true},
+    rtk6th:       {type: 'int', optional: true, indexed: true},
+    rank:         {type: 'int', optional: true, indexed: true}
   }
 };
 
@@ -23,8 +23,19 @@ const KanjiMatomeMeaningSchema = {
   primaryKey: 'keyword',
   properties: {
     keyword:      {type: 'string'},
-    type:         {type: 'int'},
-    language:     {type: 'string'},
+    type:         {type: 'int', indexed: true},
+    language:     {type: 'string', indexed: true},
+    meaning:      {type: 'string'}
+  }
+};
+
+const KanjiMatomeTangoSchema = {
+  name: 'KanjiMatomeTango',
+  properties: {
+    keyword:      {type: 'string', indexed: true},
+    tango:        {type: 'string', indexed: true},
+    hiragana:     {type: 'string', indexed: true},
+    hanViet:      {type: 'string', indexed: true},
     meaning:      {type: 'string'}
   }
 };
