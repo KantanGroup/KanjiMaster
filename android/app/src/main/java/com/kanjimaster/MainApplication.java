7178c4869c66c3c78b2442394ffe42e0a63e413b
Tran Anh Tuan <tk1cntt@gmail.com>
2016-09-17 16:22:47 +0900

    Add zip package

--- android/app/src/main/java/com/kanjimaster/MainApplication.java
+++ android/app/src/main/java/com/kanjimaster/MainApplication.java
@@ -3,6 +3,7 @@ package com.kanjimaster;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnziparchive.RNZipArchivePackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new RNZipArchivePackage(),
             new RNFetchBlobPackage(),
             new VectorIconsPackage(),
             new ReactNativeI18n(),
