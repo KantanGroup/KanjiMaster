c4205a647e9c27167a2d992a95bb65804186ef5e
Tran Anh Tuan <tk1cntt@gmail.com>
2016-09-18 18:10:12 +0900

    Remove unuse

--- App/Sagas/KanjiSagas.js
+++ App/Sagas/KanjiSagas.js
@@ -1,7 +1,5 @@
 import {take, call, put} from 'redux-saga/effects'
 import R from 'ramda'
-import Types from '../Actions/Types'
-import Actions from '../Actions/Creators'
 import I18n from 'react-native-i18n'
 
 import Toast from 'react-native-root-toast';
